221f6f0696667f1c3b4f84c602935d009540c8d4
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-17 14:30:38 +0400

    Add playground\src\features\connected\sfc-connected-component-with-action-types.tsx

---
+++ playground\src\features\connected\sfc-connected-component-with-action-types.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { createAction } from 'typesafe-actions';
+
+// Action Creators
+export const actions = {
+  ping: createAction('PING')<{ id: number }>(),
+  pong: createAction('PONG')<{ id: number }>(),
+};
+
+// Using typeof to infer dispatched action creator types in Props
+interface Props {
+  ping: typeof actions.ping;
+  pong: typeof actions.pong;
+}
+
+// Component
+const PingPongComponent: React.FC<Props> = ({ ping, pong }) => (
+  <div>
+    <button onClick={() => ping({ id: 1 })}>Ping</button>
+    <button onClick={() => pong({ id: 1 })}>Pong</button>
+  </div>
+);
+
+// Connect
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(actions, dispatch);
+
+export const PingPongConnected = connect(
+  null,
+  mapDispatchToProps
+)(PingPongComponent);
